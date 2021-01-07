5c02fcdc403e7ee4cdcb590af6b7008972b18aba
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2021-01-07 18:57:56 +0200

    Bitwig controller script skeleton.

---
+++ daw-livestream-helper.control.js
@@ -0,0 +1,22 @@
+loadAPI(12);
+
+// Remove this if you want to be able to use deprecated methods without causing script to stop.
+// This is useful during development.
+host.setShouldFailOnDeprecatedUse(true);
+
+host.defineController("com.saltiolabs", "daw-livestream-helper", "0.1", "5a8ba85f-856a-4f36-8546-7d6ef28103aa", "jasalt");
+
+function init() {
+
+   // TODO: Perform further initialization here.
+   println("daw-livestream-helper initialized!");
+}
+
+
+function flush() {
+   // TODO: Flush any output to your controller here.
+}
+
+function exit() {
+
+}
