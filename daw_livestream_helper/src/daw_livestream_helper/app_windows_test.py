2d206a0740d0fdfd6bbc499c3c570584fa3cfce9
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2021-01-12 17:53:20 +0200

    Cleanup on mac.

--- daw_livestream_helper/src/daw_livestream_helper/app_windows_test.py
+++ daw_livestream_helper/src/daw_livestream_helper/app_windows_test.py
@@ -4,16 +4,16 @@ import toga
 from toga.style.pack import CENTER, COLUMN, ROW, Pack
 import asyncio
 
-# Adding tasts fails on Windows Python 3.8.6
+# Both are failing on Windows 10, Python 3.8.6, toga 0.2.5 and 0.3.0dev25
+
+async def test_coro(args):
+    print("start task")
+    await asyncio.sleep(1)
+    print('task ran')
 
 class AsyncTest(toga.App):
-    async def atest(self):
-        print("start task")
-        await asyncio.sleep(1)
-        print('task ran')
-
     def button_handler(self, widget):
-        self.add_background_task(self.atest())
+        self.add_background_task(test_coro)
         
     async def button2_handler(self, widget):
         print("start task")
@@ -22,7 +22,7 @@ class AsyncTest(toga.App):
 
     def startup(self):
         self.main_window = toga.MainWindow(title=self.name)
-        self.button = toga.Button('add_bg_task', on_press=self.button_handler)
+        self.button = toga.Button('add_background_task', on_press=self.button_handler)
         self.button2 = toga.Button('async_handler', on_press=self.button2_handler)
 
         main_box = toga.Box(children=[self.button,self.button2])
